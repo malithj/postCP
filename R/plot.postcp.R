8e4980ee3b83af7e6240163210c19fe04fbfe4ca
malithj <malith.12@cse.mrt.ac.lk>
2016-09-02 21:02:44 +0530

    Resolving compatibility issues

--- R/plot.postcp.R
+++ R/plot.postcp.R
@@ -1,6 +1,6 @@
 plot.postcp <-
 function(x, y=NULL, xlab = "index",
-         ylab = "probability", main = "Posterior Change Point Probability Distribution", show.response = FALSE, p.col = "blue", pch = 16, p.cex = NA, m.col = "brown", m.lty = 2,
+         ylab = "probability", main = "Posterior Change Point Probability Distribution", p.col = "blue", pch = 16, p.cex = NA, m.col = "brown", m.lty = 2,
          m.lwd = 1, l.col = NA, l.lty = NA, l.lwd = NA)
 {
   # x: results from function postCP
@@ -9,26 +9,13 @@ function(x, y=NULL, xlab = "index",
   }
   n <- x$n
 
-  #Show response variable if specified
-  if (show.response) {
-    #Create plot
-    plot(c(1,n), c(min(x$response.variable), max(x$response.variable)), t = "n", xlab = xlab,
+  #Create plot
+  plot(c(1,n), c(min(x$response.variable), max(x$response.variable)), t = "n", xlab = xlab,
          ylab = "y", col = p.col, pch = pch, cex = p.cex)
-    #Change points
-    abline(v = x$cp, col = m.col, lty = m.lty, lwd = m.lwd);
-    #Add points to the graph
-    points(x$response.variable, t = 'l', col = p.col, lty = 1)
-    title(main = "Original Data")
-  } else {
-    #Create plot
-    plot(c(1,n), c(0,max(x$post.cp)), t = "n", xlab = xlab, ylab = ylab, col = p.col, pch = pch,
-         cex = p.cex)
-    #Change points
-    abline( v = x$cp, col =m.col, lty = m.lty, lwd = m.lwd);
-    #Add points to the graph
-    for (k in 1:ncol(x$post.cp))
-      points(x$post.cp[,k], t = 'l', col = k, lty = k)
-    title( main = main)
-  }
+  #Change points
+  abline(v = x$cp, col = m.col, lty = m.lty, lwd = m.lwd);
+  #Add points to the graph
+  points(x$response.variable, t = 'l', col = p.col, lty = 1)
+  title(main = "Original Data")
 
 }
